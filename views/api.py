24a0809aefcf37cd7d81400ee942c36a01b28b9c
sckroll <kimsc0714@gmail.com>
2019-12-16 07:03:17 +0900

    add API

--- views/api.py
+++ views/api.py
@@ -1,42 +1,448 @@
-from flask import Blueprint, jsonify, json
-import requests
-import pprint
-import xmltodict
+"""
+유실물 통합 조회 API
+한국기술교육대학교 서비스지향컴퓨팅및실습 텀 프로젝트
+2014136019 김성찬
+2014136030 김쾌남
 
+이점
+1. 기존에 분리되어있는 오퍼레이션을 하나로 통합,
+   하나의 API에 원하는 파라미터를 전달하는 것 만으로 사용자가 원하는 오퍼레이션을 알아서 수행
+2. 기존 API는 응답을 XML로만 제공하지만, 본 API는 JSON 형식으로 반환하므로
+   공공데이터포털 OPEN API의 대체품으로 사용 가능
+3. 특히 경찰청에 온/오프라인으로 신고한 분실물의 경우 경찰서 뿐만 아니라
+   포털기관(지하철, 백화점 등)에서 습득한 유실물 중 유사한 물품을 리스트로 제공,
+   별도로 2개 이상의 API를 사용하여 조회할 필요가 없음
+4. 분류별, 지역별, 기간별로 유실물을 조회할 경우 해당하는 코드를 알아야 API를 사용할 수 있는 단점 극복
+
+"""
+
+
+from flask import Blueprint, json, request
+from views.code_converter import location_to_code, category_to_code
+import requests
+import xmltodict
+from keys import OPEN_API_SERVICE_KEY
+
+
+# 블루프린트 등록
 api_blueprint = Blueprint('api', __name__)
 
-service_key = '%2FRpRLYKrjEbwdsYlZgkvuKZW9wgTRfDhE%2BZyDgf4MzD5OcH5%2Fx92rEl1NDzjnqrHxhcYI1WUjvRfK8n86aSNaw%3D%3D'
+# 서비스 키
+service_key = OPEN_API_SERVICE_KEY
+
+# 요청 API 호스트 URL
+host_url = 'http://apis.data.go.kr/1320000'
+
+# API 엔드 포인트
+lost_end_point = '/LostGoodsInfoInqireService'
+found_end_point = '/LosfundInfoInqireService'
+portal_end_point = '/LosPtfundInfoInqireService'
+# mobile_end_point = '/SearchMoblphonInfoInqireService'
+
+# API 요청 주소
+lost_search_np_url = host_url + lost_end_point + '/getLostGoodsInfoAccTpNmCstdyPlace'
+lost_search_cd_url = host_url + lost_end_point + '/getLostGoodsInfoAccToClAreaPd'
+lost_detail_url = host_url + lost_end_point + '/getLostGoodsDetailInfo'
+found_search_np_url = host_url + found_end_point + '/getLosfundInfoAccTpNmCstdyPlace'
+found_search_cd_url = host_url + found_end_point + '/getLosfundInfoAccToClAreaPd'
+found_detail_url = host_url + found_end_point + '/getLosfundDetailInfo'
+portal_search_np_url = host_url + portal_end_point + '/getPtLosfundInfoAccTpNmCstdyPlace'
+portal_search_cd_url = host_url + portal_end_point + '/getPtLosfundInfoAccToClAreaPd'
+portal_detail_url = host_url + portal_end_point + '/getPtLosfundDetailInfo'
+# mobile_search_url = host_url + mobile_end_point + '/getMoblphonAcctoKindAreaPeriodInfo'
+# mobile_detail_url = host_url + mobile_end_point + '/getMoblphonDetailInfo'
+
+# 분실물 / 습득물 목록 건수 디폴트값
+LOST_NUM_OF_ROWS = 10
+FOUND_NUM_OF_ROWS = 10
 
 
-@api_blueprint.route('/')
-def api_main():
-    # 1. 분실물 명칭, 보관장소 / 분류, 지역, 기간별 조회에서 atcId 추출
-    # 2. 분실물 상세정보에서 상세정보 추출
-    # 3. 분실물 명칭, 보관장소, 대/중분류, 색상, 검색시작일과 일치하는
-    #    습득물 검색 후 atcId 추출 (포털기관도 마찬가지)
-    # 4. (포털기관) 습득물 상세정보에서 상세정보 추출
-
-    url = 'http://apis.data.go.kr/1320000/LostGoodsInfoInqireService/getLostGoodsInfoAccTpNmCstdyPlace'
-
+# 해당 URL에 파라미터와 함께 요청을 보냈을 때 XML로 받은 결과를 JSON으로 변환하는 메소드
+def get_response_and_convert(url, params):
     # params를 딕셔너리로 주면 서비스키 인코딩 문제 발생 (& -> %25)
-    res_lost_goods = requests.get(
+    # 따라서 쿼리스트링에 파라미터를 연결하여 전달해야 함
+    # xml 파싱 후 json 변환을 한번 거치고 딕셔너리로 변환해야
+    # 파싱 데이터 내의 불필요한 요소를 제거할 수 있음
+
+    querystring = 'ServiceKey={0}'.format(service_key)
+    for key, val in params.items():
+        if val is not None:
+            querystring += '&{0}={1}'.format(key, val)
+
+    response = requests.get(
         url=url,
-        params='ServiceKey={0}'.format(service_key)
-        + '&LST_PLACE={0}&LST_PRDT_NM={1}'.format('', '지갑')
-        + '&pageNo={0}&numOfRows={1}'.format(1, 10)
+        params=querystring
     )
-    response_dict = xmltodict.parse(res_lost_goods.text, encoding=None)
-    response_json = json.dumps(response_dict)
-    results = json.loads(response_json)
+    res_dict = xmltodict.parse(response.text)
+    res_json = json.loads(json.dumps(res_dict))
 
-    res_json = {}
-    res_item = {}
+    if 'response' in res_json.keys():
+        res_item = res_json['response']['body']
+    else:
+        return None
 
-    res_json['items'] = []
-    res_json['items'].append(res_item)
-    res_json['lostPageNo'] = 0
-    res_json['lostNumOfRows'] = 0
-    res_json['relatedPageNo'] = 0
-    res_json['relatedNumOfRows'] = 0
+    print(res_item)
+    if 'items' in res_item.keys():
+        if res_item['items'] is not None:
+            return res_item['items']['item']
+        else:
+            return None
+    elif 'item' in res_item.keys():
+        return res_item['item']
+    else:
+        return None
 
-    return results
+
+# 기능 1. 분실물을 물품명이나 분실 장소, 물품 종류나 신고 날짜로 검색 시
+#         예상되는 습득물의 상세 정보를 리스트로 출력
+#         (분실물의 분실 장소 혹은 색상과 일치하는 신고 날짜 이후의 습득물 리스트)
+#         (명칭, 장소별 / 분류, 기간별로 분리된 API를 하나로 통합)
+@api_blueprint.route('/lostInfo')
+def lost_search():
+    # case 1. 명칭, 장소별 / 분류, 기간별 모두 빈 칸일 때
+    #         -> 명칭, 장소별 url에 pageNo, numOfRows 파라미터만 전달하여 요청
+    # case 2. 명칭, 장소별 파라미터만 채워져있을 때
+    #         -> 명칭, 장소별로 조회
+    # case 3. 분류, 기간별 파라미터만 채워져있을 때
+    #         -> 분류, 기간별로 조회
+    # case 4. 명칭, 장소별 / 분류, 기간별 모두 채워져있을 때
+    #         -> 분류, 기간별 조회 API를 베이스로 pageNo를 증가시켜가면서
+    #         -> LST_PRDT_NM이 lstPrdtNm 문자열 내에 있고 LST_PLACE가 lstPlace 문자열 내에 있는지 검사
+    #         -> 만약 lostNumOfRows만큼 찾으면 루프 종료
+    #         But, 이 경우 lostPageNo가 1이 아니라면 어떻게 할 것인가?
+    #         -> lostPageNo를 고정함으로써 해결
+
+    # API를 요청하는 데 사용할 파라미터 객체
+    params = {}
+
+    # 파라미터 입력 여부에 따라 객체에 저장
+    if request.args.get('lostPlace') is not None:
+        params['LST_PLACE'] = request.args.get('lostPlace')
+    if request.args.get('lostName') is not None:
+        params['LST_PRDT_NM'] = request.args.get('lostName')
+    if request.args.get('startDate') is not None:
+        params['START_YMD'] = request.args.get('startDate')
+    if request.args.get('endDate') is not None:
+        params['END_YMD'] = request.args.get('endDate')
+    if request.args.get('mainCategory') is not None and request.args.get('subCategory') is not None:
+        # 유실물 분류명을 코드로 변환 (상위분류, 하위분류 파라미터가 모두 채워져 있을 경우에만)
+        params['PRDT_CL_CD_01'], params['PRDT_CL_CD_02'] = \
+            category_to_code(request.args.get('mainCategory'), request.args.get('subCategory'))
+    if request.args.get('lostPlaceCode') is not None:
+        # 분실 지역명을 코드로 변환
+        params['LST_LCT_CD'] = location_to_code(request.args.get('lostPlaceCode'))
+
+    # 페이지 번호, 목록 건수 파라미터가 있으면 객체 저장, 없으면 디폴트값을 객체에 저장
+    if request.args.get('lostPageNo') is not None:
+        params['pageNo'] = request.args.get('lostPageNo')
+    else:
+        params['pageNo'] = 1
+    if request.args.get('lostNumOfRows') is not None:
+        params['numOfRows'] = request.args.get('lostNumOfRows')
+    else:
+        params['numOfRows'] = LOST_NUM_OF_ROWS
+
+    # 최종적으로 출력할 JSON
+    result = {
+        'items': [],
+        'lostPageNo': params['pageNo'],
+        'lostNumOfRows': params['numOfRows']
+    }
+    
+    # 습득물 목록 건수 파라미터가 있으면 객체 저장, 없으면 디폴트값을 객체에 저장
+    if request.args.get('foundNumOfRows') is not None:
+        result['foundNumOfRows'] = request.args.get('foundNumOfRows')
+    else:
+        result['foundNumOfRows'] = FOUND_NUM_OF_ROWS
+
+    # 분실물 리스트
+    lost_goods_list = []
+
+    if 'START_YMD' not in params.keys() and 'END_YMD' not in params.keys() and 'PRDT_CL_CD_01' not in params.keys() \
+            and 'PRDT_CL_CD_02' not in params.keys() and 'LST_LCT_CD' not in params.keys():
+        # 분류, 기간별 조회 API 파라미터가 모두 빈 칸일 때
+        # -> 파라미터 존재 여부에 상관 없이 명칭, 장소별 조회 API를 사용
+
+        lost_goods_list = get_response_and_convert(lost_search_np_url, params)
+    else:
+        # 분류, 기간별 조회 API 파라미터가 하나라도 채워져있을 때
+        # -> 파라미터 존재 여부에 상관 없이 분류, 기간별 조회 API를 사용
+        # -> 단, 명칭, 장소별 조회 API 파라미터가 하나라도 있으면 분실물명 및 장소명이 결과와 일치하는지 하나씩 검사
+
+        if 'LST_PLACE' not in params.keys() and 'LST_PRDT_NM' not in params.keys():
+            # 명칭, 장소별 조회 API 파라미터가 모두 빈 칸일 때
+
+            lost_goods_list = get_response_and_convert(lost_search_cd_url, params)
+        else:
+            # 명칭, 장소별 조회 API 파라미터가 하나라도 채워져있을 때
+            # 단, 여기서 lostPageNo는 1로 고정됨
+
+            page_no = 1
+            max_candidate_num = params['numOfRows']
+            while True:
+                params['pageNo'] = page_no
+                candidates = get_response_and_convert(lost_search_cd_url, params)
+                if candidates is None:
+                    break
+
+                for candidate in candidates:
+                    # 파라미터 존재 여부에 따라 분실물의 해당 파라미터와 일치하는지 검사
+                    if 'LST_PLACE' in params.keys() and 'LST_PRDT_NM' in params.keys():
+                        if params['LST_PLACE'] in candidate['lstPlace'] \
+                                and params['LST_PRDT_NM'] in candidate['lstPrdtNm']:
+                            lost_goods_list.append(candidate)
+                            max_candidate_num -= 1
+                    elif 'LST_PLACE' in params.keys():
+                        if params['LST_PLACE'] in candidate['lstPlace']:
+                            lost_goods_list.append(candidate)
+                            max_candidate_num -= 1
+                    elif 'LST_PRDT_NM' in params.keys():
+                        if params['LST_PRDT_NM'] in candidate['lstPrdtNm']:
+                            lost_goods_list.append(candidate)
+                            max_candidate_num -= 1
+
+                    if max_candidate_num == 0:
+                        break
+                if max_candidate_num == 0:
+                    break
+                else:
+                    page_no += 1
+
+    # 분실물 리스트 아이템의 개수
+    # (lostNumOfRows보다 작을 경우 lostNumOfRows 대신 들어갈 값)
+    num_of_item = 0
+
+    # 각 분실물의 상세정보를 리스트에 저장
+    for item in lost_goods_list:
+        params_id = {
+            'ATC_ID': item['atcId']
+        }
+
+        # 분실물 리스트의 상세정보를 요청
+        details_result = get_response_and_convert(lost_detail_url, params_id)
+
+        # 물품분류명을 상위분류명과 하위분류명으로 분리
+        product_category = details_result['prdtClNm'].split(' > ')
+
+        # 분실물 상세정보 리스트 아이템 객체
+        lost_goods_result = {
+            'id': details_result['atcId'],
+            'image': details_result['lstFilePathImg'],
+            'lostHour': details_result['lstHor'],
+            'lostLocationName': details_result['lstLctNm'],
+            'lostPlace': details_result['lstPlace'],
+            'lostPlaceSub': details_result['lstPlaceSeNm'],
+            'lostProductName': details_result['lstPrdtNm'],
+            'lostSubject': details_result['lstSbjt'],
+            'lostStateName': details_result['lstSteNm'],
+            'lostYMD': details_result['lstYmd'],
+            'orgName': details_result['orgNm'],
+            'orgId': details_result['orgId'],
+            'productCategory': product_category[0],
+            'productCategorySub': product_category[1],
+            'tel': details_result['tel'],
+            'predictedItems': []
+        }
+        # 응답 결과에 색상코드가 있다면 리스트 아이템 객체에 색상코드 추가
+        if 'clrNm' in details_result.keys():
+            lost_goods_result['color'] = details_result['clrNm']
+
+        # 습득물 조회를 위한 파라미터 객체
+        params_found = {
+            'PRDT_NM': lost_goods_result['lostProductName'],
+            'DEP_PLACE': lost_goods_result['orgName'],
+            'pageNo': 1,
+            'numOfRows': result['foundNumOfRows']
+        }
+
+        # 1. 찾은 날짜 > 잃어버린 날짜
+        # 2. 찾은 물품 = 잃어버린 물품 (정규표현식으로 어절 단위 분리, 각 어절이 포함된 물품 검사)
+        # 3. 보관 중인 장소 = 신고한 장소, else 잃어버린 장소 근처 포털기관 위치
+
+        # 분실물과 유사한 습득물 리스트 생성
+        # candidate_found = get_response_and_convert(found_search_np_url, params_found)
+        # candidate_portal = get_response_and_convert(portal_search_np_url, params_found)
+
+        page_no = 1
+        max_candidate_num = params_found['numOfRows']
+        while True:
+            params['pageNo'] = page_no
+            candidates = get_response_and_convert(found_search_np_url, params_found)
+            if candidates is None:
+                break
+
+            for candidate in candidates:
+                if int(candidate['fdYmd'].replace('-', '')) >= int(lost_goods_result['lostYMD'].replace('-', '')):
+                    lost_goods_result['predictedItems'].append(candidate)
+                    max_candidate_num -= 1
+
+                if max_candidate_num == 0:
+                    break
+            if max_candidate_num == 0:
+                break
+            else:
+                page_no += 1
+
+
+
+
+        result['items'].append(lost_goods_result)
+        num_of_item += 1
+
+    # 리스트 아이템 개수가 lostNumOfRows보다 작다면 lostNumOfRows 갱신
+    if num_of_item < result['lostNumOfRows']:
+        result['lostNumOfRows'] = num_of_item
+
+    # jsonify()로 리턴하면 한글이 유니코드 문자열로 변환되어서 출력됨
+    # https://soooprmx.com/archives/6788
+    # return jsonify(res_json)
+    return json.dumps(result, ensure_ascii=False)
+
+
+# 기능 2. 습득물을 물품명이나 습득 장소로 검색 시
+#         경찰서 뿐만 아니라 포털기관(지하철, 쇼핑몰 등)에서
+#         습득한 물품을 통합해서 상세 정보를 리스트로 출력
+#         (기존 API와는 다르게 습득물 및 포털기관 검색 결과를 통합하여 제공)
+@api_blueprint.route('/foundInfo')
+def found_search():
+    # 명칭, 장소별 / 분류, 기간별 조회는 기능 1과 동일
+    # 단, 경찰서(지구대) 습득물과 포털기관(지하철, 백화점 등) 습득물을 모두 출력해야 함
+
+    # API를 요청하는 데 사용할 파라미터 객체
+    params = {}
+
+    # 파라미터 입력 여부에 따라 객체에 저장
+    if request.args.get('foundPlace') is not None:
+        params['DEP_PLACE'] = request.args.get('foundPlace')
+    if request.args.get('foundName') is not None:
+        params['PRDT_NM'] = request.args.get('foundName')
+    if request.args.get('startDate') is not None:
+        params['START_YMD'] = request.args.get('startDate')
+    if request.args.get('endDate') is not None:
+        params['END_YMD'] = request.args.get('endDate')
+    if request.args.get('mainCategory') is not None and request.args.get('subCategory') is not None:
+        # 유실물 분류명을 코드로 변환 (상위분류, 하위분류 파라미터가 모두 채워져 있을 경우에만)
+        params['PRDT_CL_CD_01'], params['PRDT_CL_CD_02'] = \
+            category_to_code(request.args.get('mainCategory'), request.args.get('subCategory'))
+    if request.args.get('foundPlaceCode') is not None:
+        # 습득 지역명을 코드로 변환
+        params['N_FD_LCT_CD'] = location_to_code(request.args.get('foundPlaceCode'))
+    if request.args.get('colorCode') is not None:
+        params['FD_COL_CD'] = request.args.get('colorCode')
+
+    # 페이지 번호, 목록 건수 파라미터가 있으면 객체 저장, 없으면 디폴트값을 객체에 저장
+    if request.args.get('foundPageNo') is not None:
+        params['pageNo'] = request.args.get('foundPageNo')
+    else:
+        params['pageNo'] = 1
+    if request.args.get('foundNumOfRows') is not None:
+        params['numOfRows'] = request.args.get('foundNumOfRows')
+    else:
+        params['numOfRows'] = FOUND_NUM_OF_ROWS
+
+    # 최종적으로 출력할 JSON
+    result = {
+        'items': [],
+        'foundPageNo': params['pageNo'],
+        'foundNumOfRows': params['numOfRows']
+    }
+
+    # 습득물 리스트
+    found_goods_list = []
+
+    if 'START_YMD' not in params.keys() and 'END_YMD' not in params.keys() \
+            and 'PRDT_CL_CD_01' not in params.keys() and 'PRDT_CL_CD_02' not in params.keys() \
+            and 'N_FD_LCT_CD' not in params.keys() and 'FD_COL_CD' not in params.keys():
+        # 분류, 기간별 조회 API 파라미터가 모두 빈 칸일 때
+        # -> 파라미터 존재 여부에 상관 없이 명칭, 장소별 조회 API를 사용
+
+        found_goods_list = get_response_and_convert(found_search_np_url, params)
+    else:
+        # 분류, 기간별 조회 API 파라미터가 하나라도 채워져있을 때
+        # -> 파라미터 존재 여부에 상관 없이 분류, 기간별 조회 API를 사용
+        # -> 단, 명칭, 장소별 조회 API 파라미터가 하나라도 있으면 습득물명 및 장소명이 결과와 일치하는지 하나씩 검사
+
+        if 'DEP_PLACE' not in params.keys() and 'PRDT_NM' not in params.keys():
+            # 명칭, 장소별 조회 API 파라미터가 모두 빈 칸일 때
+
+            found_goods_list = get_response_and_convert(found_search_cd_url, params)
+        else:
+            # 명칭, 장소별 조회 API 파라미터가 하나라도 채워져있을 때
+            # 단, 여기서 lostPageNo는 1로 고정됨
+
+            page_no = 1
+            max_candidate_num = params['numOfRows']
+            while True:
+                params['pageNo'] = page_no
+                candidates = get_response_and_convert(found_search_cd_url, params)
+                if candidates is None:
+                    break
+
+                for candidate in candidates:
+                    # 파라미터 존재 여부에 따라 습득물의 해당 파라미터와 일치하는지 검사
+                    if 'DEP_PLACE' in params.keys() and 'PRDT_NM' in params.keys():
+                        if params['DEP_PLACE'] in candidate['depPlace'] \
+                                and params['PRDT_NM'] in candidate['fdPrdtNm']:
+                            found_goods_list.append(candidate)
+                            max_candidate_num -= 1
+                    elif 'DEP_PLACE' in params.keys():
+                        if params['DEP_PLACE'] in candidate['depPlace']:
+                            found_goods_list.append(candidate)
+                            max_candidate_num -= 1
+                    elif 'PRDT_NM' in params.keys():
+                        if params['PRDT_NM'] in candidate['fdPrdtNm']:
+                            found_goods_list.append(candidate)
+                            max_candidate_num -= 1
+
+                    if max_candidate_num == 0:
+                        break
+                if max_candidate_num == 0:
+                    break
+                else:
+                    page_no += 1
+
+    # 습득물 리스트 아이템의 개수
+    # (foundNumOfRows보다 작을 경우 foundNumOfRows 대신 들어갈 값)
+    num_of_item = 0
+
+    # 각 습득물의 상세정보를 리스트에 저장
+    for item in found_goods_list:
+        params_id = {
+            'ATC_ID': item['atcId'],
+            'FD_SN': 1
+        }
+
+        # 습득물 리스트의 상세정보를 요청
+        details_result = get_response_and_convert(found_detail_url, params_id)
+
+        # 물품분류명을 상위분류명과 하위분류명으로 분리
+        product_category = details_result['prdtClNm'].split(' > ')
+
+        # 분실물 상세정보 리스트 아이템 객체
+        found_goods_result = {
+            'id': details_result['atcId'],
+            'state': details_result['csteSteNm'],
+            'depPlace': details_result['depPlace'],
+            'image': details_result['fdFilePathImg'],
+            'foundHour': details_result['fdHor'],
+            'foundPlace': details_result['fdPlace'],
+            'foundProductName': details_result['fdPrdtNm'],
+            'foundSeq': details_result['fdSn'],
+            'foundYMD': details_result['fdYmd'],
+            'foundKeepOrgCategoryName': details_result['fndKeepOrgnSeNm'],
+            'orgId': details_result['orgId'],
+            'orgName': details_result['orgNm'],
+            'productCategory': product_category[0],
+            'productCategorySub': product_category[1],
+            'tel': details_result['tel'],
+            'uniq': details_result['uniq']
+        }
+
+        result['items'].append(found_goods_result)
+        num_of_item += 1
+
+    # 리스트 아이템 개수가 foundNumOfRows보다 작다면 foundNumOfRows 갱신
+    if num_of_item < result['foundNumOfRows']:
+        result['foundNumOfRows'] = num_of_item
+
+    return json.dumps(result, ensure_ascii=False)
---
+++ views/code_converter.py
@@ -0,0 +1,218 @@
+# 분실 / 습득 지역명을 지역코드로 변환하는 메소드 (도, 광역시 단위만 가능)
+def location_to_code(location_name):
+    if '서울' in location_name:
+        location_code = 'LCA000'
+    elif '강원' in location_name:
+        location_code = 'LCH000'
+    elif '경기' in location_name:
+        location_code = 'LCI000'
+    elif '경상남도' in location_name or '경남' in location_name:
+        location_code = 'LCJ000'
+    elif '경상북도' in location_name or '경북' in location_name:
+        location_code = 'LCK000'
+    elif '광주' in location_name:
+        location_code = 'LCQ000'
+    elif '대구' in location_name:
+        location_code = 'LCR000'
+    elif '대전' in location_name:
+        location_code = 'LCS000'
+    elif '부산' in location_name:
+        location_code = 'LCT000'
+    elif '울산' in location_name:
+        location_code = 'LCU000'
+    elif '인천' in location_name:
+        location_code = 'LCV000'
+    elif '전라남도' in location_name or '전남' in location_name:
+        location_code = 'LCL000'
+    elif '전라북도' in location_name or '전북' in location_name:
+        location_code = 'LCM000'
+    elif '충청남도' in location_name or '충남' in location_name:
+        location_code = 'LCN000'
+    elif '충청북도' in location_name or '충북' in location_name:
+        location_code = 'LCO000'
+    elif '제주' in location_name:
+        location_code = 'LCP000'
+    elif '세종' in location_name:
+        location_code = 'LCW000'
+    elif '해외' in location_name:
+        location_code = 'LCF000'
+    else:
+        location_code = 'LCE000'
+
+    return location_code
+
+
+# 유실물 분류명을 분류코드로 변환하는 메소드
+def category_to_code(main_category_name, sub_category_name):
+    if main_category_name == '가방':
+        main_category_code = 'PRA000'
+        if '여성' in sub_category_name:
+            sub_category_code = 'PRA100'
+        elif '남성' in sub_category_name:
+            sub_category_code = 'PRA200'
+        else:
+            sub_category_code = 'PRA300'
+    elif main_category_name == '귀금속':
+        main_category_code = 'PRO000'
+        if sub_category_name == '반지':
+            sub_category_code = 'PRO100'
+        elif sub_category_name == '목걸이':
+            sub_category_code = 'PRO200'
+        elif sub_category_name == '귀걸이':
+            sub_category_code = 'PRO300'
+        elif sub_category_name == '시계':
+            sub_category_code = 'PRO400'
+        else:
+            sub_category_code = 'PRO500'
+    elif main_category_name == '도서용품':
+        main_category_code = 'PRB000'
+        if sub_category_name == '학습서적':
+            sub_category_code = 'PRB100'
+        elif sub_category_name == '소설':
+            sub_category_code = 'PRB200'
+        elif sub_category_name == '컴퓨터서적':
+            sub_category_code = 'PRB300'
+        elif sub_category_name == '만화책':
+            sub_category_code = 'PRB400'
+        else:
+            sub_category_code = 'PRB500'
+    elif main_category_name == '서류':
+        main_category_code = 'PRC000'
+        if sub_category_name == '서류':
+            sub_category_code = 'PRC100'
+        else:
+            sub_category_code = 'PRC200'
+    elif main_category_name == '산업용품':
+        main_category_code = 'PRD000'
+        sub_category_code = 'PRD100'
+    elif main_category_name == '쇼핑백':
+        main_category_code = 'PRQ000'
+        sub_category_code = 'PRQ100'
+    elif main_category_name == '스포츠용품':
+        main_category_code = 'PRE000'
+        sub_category_code = 'PRE100'
+    elif main_category_name == '악기':
+        main_category_code = 'PRR000'
+        if sub_category_name == '건반악기':
+            sub_category_code = 'PRR100'
+        elif sub_category_name == '관악기':
+            sub_category_code = 'PRR200'
+        elif sub_category_name == '타악기':
+            sub_category_code = 'PRR300'
+        elif sub_category_name == '현악기':
+            sub_category_code = 'PRR400'
+        else:
+            sub_category_code = 'PRR900'
+    elif main_category_name == '유가증권':
+        main_category_code = 'PRM000'
+        if sub_category_name == '어음':
+            sub_category_code = 'PRM100'
+        elif sub_category_name == '상품권':
+            sub_category_code = 'PRM200'
+        elif sub_category_name == '채권':
+            sub_category_code = 'PRM300'
+        else:
+            sub_category_code = 'PRM400'
+    elif main_category_name == '의류':
+        main_category_code = 'PRK000'
+        if '여성' in sub_category_name:
+            sub_category_code = 'PRK100'
+        elif '남성' in sub_category_name:
+            sub_category_code = 'PRK200'
+        elif '아기' in sub_category_name:
+            sub_category_code = 'PRK300'
+        else:
+            sub_category_code = 'PRK400'
+    elif main_category_name == '자동차':
+        main_category_code = 'PRF000'
+        if sub_category_name == '자동차열쇠':
+            sub_category_code = 'PRF100'
+        elif sub_category_name == '네비게이션' or sub_category_name == '내비게이션':
+            sub_category_code = 'PRF200'
+        elif sub_category_name == '자동차번호판':
+            sub_category_code = 'PRF300'
+        elif sub_category_name == '임시번호판':
+            sub_category_code = 'PRF500'
+        else:
+            sub_category_code = 'PRF400'
+    elif main_category_name == '전자기기':
+        main_category_code = 'PRG000'
+        if sub_category_name == 'PMP':
+            sub_category_code = 'PRG100'
+        elif sub_category_name == 'MP3':
+            sub_category_code = 'PRG200'
+        elif sub_category_name == 'PDA':
+            sub_category_code = 'PRG300'
+        elif sub_category_name == '카메라':
+            sub_category_code = 'PRG400'
+        elif sub_category_name == '전자수첩':
+            sub_category_code = 'PRG500'
+        else:
+            sub_category_code = 'PRG600'
+    elif main_category_name == '지갑':
+        main_category_code = 'PRH000'
+        if '여성' in sub_category_name:
+            sub_category_code = 'PRH100'
+        elif '남성' in sub_category_name:
+            sub_category_code = 'PRH200'
+        else:
+            sub_category_code = 'PRH300'
+    elif main_category_name == '증명서':
+        main_category_code = 'PRN000'
+        if sub_category_name == '신분증':
+            sub_category_code = 'PRN100'
+        elif sub_category_name == '면허증':
+            sub_category_code = 'PRN200'
+        elif sub_category_name == '여권':
+            sub_category_code = 'PRN300'
+        else:
+            sub_category_code = 'PRN400'
+    elif main_category_name == '컴퓨터':
+        main_category_code = 'PRI000'
+        if '삼성' in sub_category_name:
+            sub_category_code = 'PRI100'
+        elif 'LG' in sub_category_name:
+            sub_category_code = 'PRI200'
+        elif '삼보' in sub_category_name:
+            sub_category_code = 'PRI300'
+        elif 'HP' in sub_category_name:
+            sub_category_code = 'PRI500'
+        else:
+            sub_category_code = 'PRI400'
+    elif main_category_name == '카드':
+        main_category_code = 'PRP000'
+        if '신용' in sub_category_name or '체크' in sub_category_name:
+            sub_category_code = 'PRP100'
+        elif sub_category_name == '일반카드':
+            sub_category_code = 'PRP200'
+        else:
+            sub_category_code = 'PRP300'
+    elif main_category_name == '현금':
+        main_category_code = 'PRL000'
+        if sub_category_name == '현금':
+            sub_category_code = 'PRL100'
+        elif sub_category_name == '수표':
+            sub_category_code = 'PRL200'
+        elif sub_category_name == '외화':
+            sub_category_code = 'PRL400'
+        else:
+            sub_category_code = 'PRL300'
+    elif main_category_name == '휴대폰':
+        main_category_code = 'PRJ000'
+        if '모토로라' in sub_category_name:
+            sub_category_code = 'PRJ600'
+        elif '삼성' in sub_category_name:
+            sub_category_code = 'PRJ100'
+        elif 'LG' in sub_category_name:
+            sub_category_code = 'PRJ200'
+        elif '스카이' in sub_category_name:
+            sub_category_code = 'PRJ300'
+        elif '아이폰' in sub_category_name:
+            sub_category_code = 'PRJ400'
+        else:
+            sub_category_code = 'PRJ500'
+    else:
+        main_category_code = 'PRZ000'
+        sub_category_code = 'PRZ100'
+
+    return main_category_code, sub_category_code
