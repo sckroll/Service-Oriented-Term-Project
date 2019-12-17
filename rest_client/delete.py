e96c484610c0a5bb9170f879b023112524909c20
sckroll <kimsc0714@gmail.com>
2019-12-18 04:28:11 +0900

    modified API and styles

--- rest_client/delete.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-if __name__ == "__main__":
-    res = requests.delete(
-        url='http://127.0.0.1:8000/resource/t6'
-    )
-
-    print(res.status_code)
-    print(res.json())
